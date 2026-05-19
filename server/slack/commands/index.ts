3d2fe260a090296a710c792b4e1dcf817dc173cd
Anirudh Sriram <hello@techwithanirudh.com>
2026-05-19 00:50:06 +0000

    refactor: update biome configuration and clean up command index

--- server/slack/commands/index.ts
+++ server/slack/commands/index.ts
@@ -1,6 +1,5 @@
 import { handleCommand } from './handler';
 
 export const commands = [
-  // biome-ignore lint/performance/useTopLevelRegex: pattern is module-local
   { pattern: /^\/gork(?:-\w+)?$/, execute: handleCommand },
 ];
